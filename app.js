a2b1e4a5e07de70e7f6e647a243931c6eae7faca
MicaelaVizcarra <micaelavizcarra97@gmail.com>
2022-08-25 15:45:27 -0300

    Agregue flexbox y arregle algunos errores

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const path = require('path');
 
 
 app.use(express.static('public'));
-app.listen(port, () => console.log('Server running in http://localhost:' + port))
+
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+
+
+app.listen(port, () => console.log('Server running in http://localhost:' + port))
